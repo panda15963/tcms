374a847c102fe717d5c87609a667e621fb414643
최민석 <seok7521@gmail.com>
2024-07-24 16:22:06 +0900

    edit code in alerts

--- src/components/alerts/CopiedCopletion.jsx
+++ src/components/alerts/CopiedCopletion.jsx
@@ -3,18 +3,18 @@ import { FaCheck } from 'react-icons/fa6';
 export default function CopiedCopletion() {
   return (
     <div
-      className="fixed left-1/2 transform -translate-x-1/2 bg-green-200 text-white p-5 rounded-md shadow-md mt-20 z-50"
+      className="fixed left-1/2 transform -translate-x-1/2 bg-green-200 text-white p-2 rounded-md shadow-md mt-20 z-50"
       role="alert"
     >
       <div className="flex items-center justify-center">
         <div className="flex-shrink-0">
           <FaCheck
             aria-hidden="true"
-            className="h-5 w-5 text-green-500 text-lg"
+            className="h-5 w-5 text-green-500 text-md"
           />
         </div>
         <div className="ml-3">
-          <p className="text-lg font-medium text-green-800">
+          <p className="text-md font-medium text-green-800">
             Successfully Copied!
           </p>
         </div>
--- src/components/alerts/CopiedError.jsx
+++ src/components/alerts/CopiedError.jsx
@@ -3,18 +3,18 @@ import { FaExclamationCircle } from 'react-icons/fa';
 export default function CopiedError({ errorMessage }) {
   return (
     <div
-      className="fixed left-1/2 transform -translate-x-1/2 bg-red-200 text-white p-5 rounded-md shadow-md mt-20 z-50"
+      className="fixed left-1/2 transform -translate-x-1/2 bg-red-200 text-white p-2 rounded-md shadow-md mt-20 z-50"
       role="alert"
     >
       <div className="flex items-center justify-center">
         <div className="flex-shrink-0">
           <FaExclamationCircle
             aria-hidden="true"
-            className="h-5 w-5 text-red-500 text-lg"
+            className="h-5 w-5 text-red-500 text-md"
           />
         </div>
         <div className="ml-3">
-          <p className="text-lg font-medium text-red-800">{errorMessage}</p>
+          <p className="text-md font-medium text-red-800">{errorMessage}</p>
         </div>
       </div>
     </div>
