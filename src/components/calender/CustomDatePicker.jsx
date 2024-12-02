c15814f676a3f3001f1f56c141c9c4ab1d4e015f
john <john@modim.com>
2024-12-02 09:39:32 +0900

    DatePicker 사이즈 변경

--- src/components/calender/CustomDatePicker.jsx
+++ src/components/calender/CustomDatePicker.jsx
@@ -18,10 +18,20 @@ const DateRangePicker = ({ startsDate, endsDate, startDate, endDate }) => {
     }
   }, [endDate, endsDate]);
 
+  const CustomInput = React.forwardRef(({ value, onClick }, ref) => (
+    <button
+      ref={ref}
+      onClick={onClick}
+      className="border py-1 border-black text-center rounded-md w-32 h-10 box-border focus:outline-none focus:ring-0"
+    >
+      {value || '날짜 선택'}
+    </button>
+  ));
+
   return (
     <div className="flex items-center">
       {/* Start Date Picker */}
-      <div className=" flex items-center gap-2 ">
+      <div className=" flex items-center gap-0 ">
         <label className="text-sm font-semibold text-white whitespace-nowrap">
           {/* 시작 날짜 */}
           {/* {t('DateRangePicker.StartDate')} */}
@@ -41,7 +51,7 @@ const DateRangePicker = ({ startsDate, endsDate, startDate, endDate }) => {
       <span className="pl-2 text-lg text-white font-bold">~</span>
 
       {/* End Date Picker */}
-      <div className="pl-2 flex items-center gap-2">
+      <div className="pl-2 flex items-center gap-0">
         <label className="text-sm font-semibold text-white whitespace-nowrap">
           {/* 종료 날짜 */}
           {/* {t('DateRangePicker.EndDate')} */}
